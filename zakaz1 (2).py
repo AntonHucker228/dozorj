21dcf6dc07e28d231783432f6feaf161768bf353
AntonHucker228 <emiratovfork@gmail.com>
2026-05-15 17:53:29 +0500

    Update zakaz1 (2).py

--- zakaz1 (2).py
+++ zakaz1 (2).py
@@ -478,7 +478,7 @@ router = Router()
 user_referrers = {}
 # _ _ _ Покупка подарка _ _ _
 @router.callback_query(F.data == "for_10")
-async def gifts_for_10(callback: CallbackQuery, message: Message):
+async def gifts_for_10(message: Message, callback: CallbackQuery):
     user = db.get_user(user_id)
     username = db.get_user(user_name)
     await message.edit_message("Здесь вы можете приобрести подарки 🧸/💝 всего за 10 звезд⭐\n\nВыберите подарок, а затем проведите оплату!", reply_markup=choise_gift())
